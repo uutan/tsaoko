d48fbe49840f223a84a5d96a9ae787dcb7377ae4
UUTAN <uutan@qq.com>
2014-11-06 20:46:35 +0800

    a

---
+++ themes/caoguo/views/site/come.php
@@ -0,0 +1,18 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<meta http-equiv="X-UA-Compatible" content="IE=10;IE=9;IE=8;IE=7;IE=EDGE">
+<meta charset="utf-8">
+<title>草果网</title>
+</head>
+<body>
+
+<style type="text/css">
+html,body{ background: #f1f1f1;}
+p{ text-align: center; line-height: 300px; font-size:14px;}
+</style>
+
+<p>公司官网改版中...</p>
+
+</body>
+</html>
